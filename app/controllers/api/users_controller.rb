5a49fe30d0fac3a5ac8a28b9fb8607b34018737b
zrahat <zarifrahat123@gmail.com>
2020-06-02 06:50:02 -0400

    initial user auth commit

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,24 @@
+class Api::UsersController < ApplicationController
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login(@user)
+            redirect_to poems_url 
+        else
+            flash[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+    #FIGVAPER
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :email, uniqueness: true
+  validates :password, length: { minimum: 6 , allow_nil: true} 
+
+  attr_reader :password
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+        @user = User.find_by(username: username)
+        return nil if @user.nil?
+        @user.is_password?(password) ? @user : nil
+    end
+
+    def is_password?(password)
+        bcrypt_obj = BCrypt::Password.new(self.password_digest)
+        bcrypt_obj.is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.save!
+        self.session_token
+    end
+end
